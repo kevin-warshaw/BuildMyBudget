1c49d8f192e7d2c1484f2fd71b3487256595e3c5
kevin-warshaw <31418628+kevin-warshaw@users.noreply.github.com>
2021-06-02 23:34:00 -0400

    VIEW-0001: Initial setup to allow CRUD operations for models

--- BuildMyBudget_backend/BuildMyBudget/admin.py
+++ BuildMyBudget_backend/BuildMyBudget/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Entry
+from .models import Entry, Category
 
 # Register your models here.
+admin.site.register(Category)
 admin.site.register(Entry)
---
+++ BuildMyBudget_backend/BuildMyBudget/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+from . import models
+
+class CustomUserSerializer(serializers.ModelSerializer):
+  class Meta:
+        fields = "__all__"
+        model = models.CustomUser
+
+class CategorySerializer(serializers.ModelSerializer):
+  class Meta:
+        fields = "__all__"
+        model = models.Category
+
+class DetailedCategorySerializer(serializers.ModelSerializer):
+  category = CategorySerializer()
+
+  class Meta:
+        fields = "__all__"
+        model = models.Entry
+
+class EntrySerializer(serializers.ModelSerializer):
+  class Meta:
+        fields = "__all__"
+        model = models.Entry
