a665626305b937814cde7f2eaf9cff75a2f987e1
kevin-warshaw <31418628+kevin-warshaw@users.noreply.github.com>
2021-07-23 00:38:16 -0400

    REACT-0002: Updated imports to resolve compiler error

--- BuildMyBudget_backend/BuildMyBudget/views.py
+++ BuildMyBudget_backend/BuildMyBudget/views.py
@@ -1,7 +1,7 @@
 from rest_framework.permissions import AllowAny
 from rest_framework.viewsets import ModelViewSet
 from .models import CustomUser, Category, Entry
-from .serializers import CustomUserSerializer, CategorySerializer, EntrySerializer, DetailedCategorySerializer
+from .serializers import CustomUserSerializer, EntrySerializer, ParentCategorySerializer
 
 class CustomUserViewSet(ModelViewSet):
     queryset = CustomUser.objects.all()
